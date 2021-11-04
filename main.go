898630bd758a0356687a64d5030aadbfafcb3bef
Ram <ramakumarja@gmail.com>
2021-11-04 14:19:01 +0530

    added a http server

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", helloWorld)
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
+
 func main() {
-	fmt.Println("Hello World")
+	handleRequests()
 }
